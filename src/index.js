ff43c384054cc28c0df1440f78ba6412b2f27eb3
Parthiv <parmarparthiv77@gmail.com>
2024-09-15 11:00:26 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import SharekhanApi from "./sharekhan-api";
+import WebSocket from "./websockets";
+
+const _SharekhanApi = SharekhanApi;
+export { _SharekhanApi as SharekhanApi };
+
+const _WebSocket = WebSocket;
+export { _WebSocket as WebSocket };
